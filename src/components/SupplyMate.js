565c9b6ed6bc06df5af4fd1079eb917c909fc2e7
Chase Fite <chasef7031@gmail.com>
2020-03-13 14:31:57 -0500

    initial react file structure setup

---
+++ src/components/SupplyMate.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class SupplyMate extends Component {
+
+    render() {
+        return (
+            <>
+                <div>Hi there</div>
+            </>
+        )
+    }
+}
+
+export default SupplyMate
